40a92d75af1b948ae9b455e35d89076c5f156d5b
ytasnime <tasnime.yahiaoui@amvs.fr>
2022-11-04 15:59:01 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<section>
+	<h1>Voulez vous donner votre avis ?</h1>
+	<a href="/smiley">répondre</a>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	
+</style>
---
+++ src/routes/smiley/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+
+    let question = "Quelle est votre satisfaction globale au regard de l’accueil qui vous a été réservé ?";
+
+</script>
+
+<main>
+    <h1 style="text-align: center">{question}</h1>
+    <div class="divSmileys">
+        <button class="smiley">
+            <img  class="image5" src="happy.png" alt="Emoji vert">
+        </button>
+        <button class="smiley">
+            <img  class="image4" src="happy.png" alt="Emoji vert clair">
+        </button>
+        <button class="smiley">
+            <img  class="image3" src="happy.png" alt="Emoji jaune">
+        </button>
+        <button class="smiley">
+            <img  class="image2" src="happy.png" alt="Emoji orange">
+        </button>
+        <button class="smiley">
+            <img  class="image1" src="happy.png" alt="Emoji rouge">
+        </button>
+    </div>
+
+</main>
+
+<style>
+
+    .smiley{
+        background-color: transparent;
+        width: 15%;
+        height: auto;
+        border:0;
+    }
+
+    img{
+        max-width: 100%;
+        height: auto;
+        width: auto\9;
+    }
+
+
+
+    .divSmileys{
+        justify-content: center;
+        display: flex;
+        padding-top: 10%;
+    }
+
+</style>
